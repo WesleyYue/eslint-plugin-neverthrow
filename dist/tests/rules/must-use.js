b0ffd6d786389156aabcb4494afbc089f6a06793
Wesley Yue <wesleyiyue@gmail.com>
2023-05-26 18:10:04 -0700

    Add dist and extra test

--- dist/tests/rules/must-use.js
+++ dist/tests/rules/must-use.js
@@ -1,10 +1,13 @@
-import { TSESLint } from '@typescript-eslint/experimental-utils';
-import rule from '../../src/rules/must-use-result';
-import { MessageIds } from '../../src/utils';
-
-function injectResult(name: string, text: string) {
-  return (
-    `// ${name}
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const experimental_utils_1 = require("@typescript-eslint/experimental-utils");
+const must_use_result_1 = __importDefault(require("../../src/rules/must-use-result"));
+const utils_1 = require("../../src/utils");
+function injectResult(name, text) {
+    return (`// ${name}
 declare interface ErrorConfig {
   withStackTrace: boolean;
 }
@@ -70,154 +73,107 @@ declare function getResult(): Result<string, Error>
 declare function getNormal(): number
 const obj: { get: () => Result<string, Error> }
 
-` + text
-  );
+` + text);
 }
-
-new TSESLint.RuleTester({
-  parser: require.resolve('@typescript-eslint/parser'),
-}).run('must-use-result', rule, {
-  valid: [
-    injectResult(
-      'call unwrapOr',
-      `
+new experimental_utils_1.TSESLint.RuleTester({
+    parser: require.resolve('@typescript-eslint/parser'),
+}).run('must-use-result', must_use_result_1.default, {
+    valid: [
+        injectResult('call unwrapOr', `
       const result = getResult()
 
       result.unwrapOr()
-    `
-    ),
-    injectResult(
-      'call unwrapOr after some methods',
-      `
+    `),
+        injectResult('call unwrapOr after some methods', `
       const result = getResult()
 
       result.map(() => {}).unwrapOr('')
-    `
-    ),
-    injectResult(
-      'Call match',
-      `
+    `),
+        injectResult('Call match', `
       const result = getResult()
       result.match(() => {}, () => {})
-    `
-    ),
-    injectResult(
-      'Return result from function',
-      `
+    `),
+        injectResult('Return result from function', `
       function main() {
         return getResult().map(() => {})
       }
-    `
-    ),
-    injectResult(
-      'Return result from an arrow function',
-      `
+    `),
+        injectResult('Return result from an arrow function', `
       const main = () => getResult().map(() => {})
-    `
-    ),
-    injectResult(
-      'Call a normal function',
-      `
+    `),
+        injectResult('Call a normal function', `
       getNormal()
-    `
-    ),
-    injectResult(
-      'Handle with isErr()',
-      `
+    `),
+        injectResult('Handle with isErr()', `
       const result = getResult();
 
       if (result.isErr()) {
         console.log("okay")
       }
-      `
-    ),
-    `// Without definitions
+      `),
+        `// Without definitions
       getNormal()
     `,
-  ],
-  invalid: [
-    {
-      code: injectResult(
-        'only assignment',
-        `
+    ],
+    invalid: [
+        {
+            code: injectResult('only assignment', `
         const result = getResult()
-      `
-      ),
-      errors: [{ messageId: MessageIds.MUST_USE }],
-    },
-    {
-      code: injectResult(
-        'Call map for result',
-        `
+      `),
+            errors: [{ messageId: utils_1.MessageIds.MUST_USE }],
+        },
+        {
+            code: injectResult('Call map for result', `
         const result = getResult();
         result.map(() => {})
-      `
-      ),
-      errors: [
-        { messageId: MessageIds.MUST_USE },
-        { messageId: MessageIds.MUST_USE },
-      ],
-    },
-    {
-      code: injectResult(
-        'only call',
-        `
+      `),
+            errors: [
+                { messageId: utils_1.MessageIds.MUST_USE },
+                { messageId: utils_1.MessageIds.MUST_USE },
+            ],
+        },
+        {
+            code: injectResult('only call', `
         getResult()
-      `
-      ),
-      errors: [{ messageId: MessageIds.MUST_USE }],
-    },
-    {
-      code: injectResult(
-        'call external function',
-        `
+      `),
+            errors: [{ messageId: utils_1.MessageIds.MUST_USE }],
+        },
+        {
+            code: injectResult('call external function', `
         const v = getResult()
         externaFunction(v)
-      `
-      ),
-      errors: [{ messageId: MessageIds.MUST_USE }],
-    },
-    {
-      code: injectResult(
-        'made call from object',
-        `
+      `),
+            errors: [{ messageId: utils_1.MessageIds.MUST_USE }],
+        },
+        {
+            code: injectResult('made call from object', `
         obj.get()
-      `
-      ),
-      errors: [{ messageId: MessageIds.MUST_USE }],
-    },
-    {
-      code: injectResult(
-        'none of the handle methods is called',
-        `
+      `),
+            errors: [{ messageId: utils_1.MessageIds.MUST_USE }],
+        },
+        {
+            code: injectResult('none of the handle methods is called', `
         getResult().unwrapOr
-      `
-      ),
-      errors: [{ messageId: MessageIds.MUST_USE }],
-    },
-    {
-      code: injectResult(
-        'called inside a function',
-        `
+      `),
+            errors: [{ messageId: utils_1.MessageIds.MUST_USE }],
+        },
+        {
+            code: injectResult('called inside a function', `
         function main() {
           getResult().map(() => {})
         }
-      `
-      ),
-      errors: [
-        { messageId: MessageIds.MUST_USE },
-        { messageId: MessageIds.MUST_USE },
-      ],
-    },
-    {
-      code: injectResult(
-        'any if statement follows',
-        `
+      `),
+            errors: [
+                { messageId: utils_1.MessageIds.MUST_USE },
+                { messageId: utils_1.MessageIds.MUST_USE },
+            ],
+        },
+        {
+            code: injectResult('any if statement follows', `
         const result = getResult()
         if (true) {console.log("okay")}
-        `
-      ),
-      errors: [{ messageId: MessageIds.MUST_USE }],
-    },
-  ],
+        `),
+            errors: [{ messageId: utils_1.MessageIds.MUST_USE }],
+        },
+    ],
 });
